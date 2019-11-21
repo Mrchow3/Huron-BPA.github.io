59c044030fae64897408a14af584e02b62ac6b8f
Peter Stenger <peter.a.stenger@gmail.com>
2019-11-20 23:28:00 -0500

    rearrange

--- js/include.js
+++ js/include.js
@@ -1,6 +1,13 @@
 $(function () {
     jQuery.each($('[data-load]'), function () {
         // this refers to the element
-        $(this).load("/pages/" + $(this).data("load"))
+        $(this).load("/" + $(this).data("load"))
     })
 })
+
+$(function () {
+    $(document).scroll(function () {
+        // toggle if greater than height
+        $(".navbar").toggleClass('navbar-dark bg-dark see-through transition', $(window).scrollTop() > $(window).height() * .8);
+    });
+});
