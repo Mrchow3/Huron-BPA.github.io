f9b7fdfdcf800d539f1df2fde310de922d0bac61
Peter Stenger <peter.a.stenger@gmail.com>
2019-11-27 00:10:49 -0500

    yeah ok

--- scripts/watermark.py
+++ scripts/watermark.py
@@ -1,22 +1,40 @@
 #!/usr/bin/env python
 import os, csv, requests
 from os.path import dirname, join, abspath
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+import subprocess
+
+scope = ['https://spreadsheets.google.com/feeds',
+         'https://www.googleapis.com/auth/drive']
+
+credentials = ServiceAccountCredentials.from_json_keyfile_name(
+    join(dirname(abspath(__file__)), 'creds.json'), scope)
+
+gc = gspread.authorize(credentials)
+worksheet = gc.open_by_url(
+    'https://docs.google.com/spreadsheets/d/1Gvx-VLYMUNGL-XF_SbaBiHNCO9jpsGmCFwh56LEv39s/edit#gid=0').sheet1
 website = dirname(dirname(abspath(__file__)))
-with open(join(website, "other", "images.csv")) as csv_file:
-    lines = csv.DictReader(csv_file)
-    for line in lines:
-        if not line['filename'] or not line['url']:
-            continue
-        ending = line['url'].split('.')[-1].lower()
-        file_path = join(website, "assets", line['filename'] + f'.{ending}')
-        citation = line['citation']
-        with open(file_path, 'wb') as write:
-            write.write(requests.get(line['url']).content)
-            print(f'downloaded {line["filename"]}.{ending}')
-            if citation is not None:
-                print('adding citation', citation)
-                citation = citation.replace('"',"\\\"",-1)
-                os.system(f'magick convert "{file_path}" -fill white \
--font Times-New-Roman -pointsize 24 \
--undercolor "#00000080" -gravity South -annotate +0+0 "{citation}" "{file_path}"')
-        print(line["url"])
+items = worksheet.get_all_records()
+for row in items:
+    if not row["filename"] or not row["url"]:
+        continue
+
+    ending = row["url"].split('.')[-1].lower()
+    file_path = join(website, "assets", row["filename"] + f'.{ending or "jpg"}')
+    file_path_caption = join(website, "assets", row["filename"] + f'_caption.{ending or "jpg"}')
+    citation = row['citation']
+    with open(file_path, 'wb') as write:
+        write.write(requests.get(row["url"]).content)
+        print(f'downloaded {row["filename"]}.{ending or "jpg"}')
+        if citation is not None:
+            print('adding citation', citation)
+            # citation = citation.replace('"',"\\\"",-1)
+            part1 = f'magick identify -format %w,%h "{file_path}"'
+            part2 = 'magick convert -background "#00000080" -fill white -gravity center \
+-font Times-New-Roman -size {}x -pointsize {}  caption:"{}" \
+"{}" +swap -gravity North -composite "{}"'
+            out = subprocess.check_output(part1, shell=True)
+            num1, num2 = out.decode().split(',')
+            height = int(int(num2) / 30)
+            os.system(part2.format(int(num1), height, citation.replace('"','\\"'), file_path, file_path_caption))
